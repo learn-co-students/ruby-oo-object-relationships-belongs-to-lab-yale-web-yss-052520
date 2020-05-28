8914bb0f1c41655cb803ca710428b1d573ce67c1
ev275 <enrique.valencia@yale.edu>
2020-05-28 12:48:53 -0500

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,19 @@
+require "pry"
+require_relative "artist.rb"
+
+class Song
+    attr_accessor :title, :artist
+    
+    # def initialize(title,artist)
+    #     @title = title
+    #     @artist = artist
+    # end
+end
+
+# strokes = Artist.new("The Strokes")
+
+# selfless = Song.new("Selfless", strokes)
+
+
+# binding.pry
+# 0
